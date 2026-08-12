45d56691ccd35d85b152bc07606b74b4215df20b
lucifer1570 <kspg624@gmail.com>
2026-08-12 22:58:55 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const fs          = require('fs');
 // ============================================================
 //  CONFIG
 // ============================================================
-const BOT_TOKEN    = process.env.BOT_TOKEN || "8756624614:AAF81fxfThFxhnsU7rlfTKZaKW7_M6caa3Y"
+const BOT_TOKEN    = process.env.BOT_TOKEN || "8999335291:AAH0JGwDXTCwCkiuY0z4B3nOAwicEgWS2bo"
 const OWNER_ID     = 1865939951;
 const OWNER_PASS   = "praveensaran";
 const ADMIN_HANDLE = "@lucifer1570";
